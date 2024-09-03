2e10e50f1a059e2a270b9b99ad03ac61773258ef
Rayane Tchabodi <139383014+Ygryan360@users.noreply.github.com>
2024-09-03 15:45:14 +0000

    Finito la front end

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,15 +2,79 @@ function refreshpage() {
   location.reload();
 }
 
+function gotToLecturePage () {
+  let lectureBtn = document.getElementById("lectureBtn");
+  lectureBtn.addEventListener("click", () => {
+    document.location = "/course.html";
+  });
+}
+
 // setInterval(refreshpage, 5000);
 let goToTopBtn = document.getElementById("scrollToTop");
-window.onscroll =  () =>{
-  if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+window.onscroll = () => {
+  if (
+    document.body.scrollTop > 500 ||
+    document.documentElement.scrollTop > 500
+  ) {
     goToTopBtn.style.display = "block";
   } else {
     goToTopBtn.style.display = "none";
   }
-}
-  function scrollToTop() {
+};
+function scrollToTop() {
   window.scrollTo({ top: 0, behavior: "smooth" });
 }
+
+
+/**
+ * Animations
+ */
+document.addEventListener("DOMContentLoaded", function () {
+  const fadeIns = document.querySelectorAll(".fade-in");
+  console.log(fadeIns);
+  
+
+  // Vérifie que l'observateur fonctionne bien
+  if ("IntersectionObserver" in window) {
+    const observer = new IntersectionObserver(
+      (entries, observer) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            entry.target.classList.add("fade-in-visible");
+            observer.unobserve(entry.target);
+          }
+        });
+      },
+      { threshold: 0.1 }
+    ); // Se déclenche quand 10% de l'élément est visible
+
+    fadeIns.forEach((fadeIn) => {
+      observer.observe(fadeIn);
+    });
+  } else {
+    // Fallback si IntersectionObserver n'est pas supporté
+    fadeIns.forEach((fadeIn) => {
+      fadeIn.classList.remove("fade-in")
+      fadeIn.classList.add("fade-in-visible");
+    });
+  }
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const elements = document.querySelectorAll(".fade-in-up");
+
+  const observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("show");
+        }
+      });
+    },
+    { threshold: 0.1 }
+  );
+
+  elements.forEach((el) => {
+    observer.observe(el);
+  });
+});
